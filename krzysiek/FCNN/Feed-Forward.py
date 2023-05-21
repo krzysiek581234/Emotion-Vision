c8b955ca2a70338a3b3a37e3f8a305f3cc09f3b3
krzysiek581234 <krzysztof.madajczakbiznes@gmail.com>
2023-05-22 01:37:52 +0200

    resnet 74%+ feedforward 44%

--- krzysiek/FCNN/Feed-Forward.py
+++ krzysiek/FCNN/Feed-Forward.py
@@ -7,27 +7,29 @@ import torchvision.transforms as transforms
 import matplotlib.pyplot as plt
 from torch.utils.data import Dataset
 from NeuralNet import NeuralNet
+from krzysiek.FCNN.jcoopdateread import FER2013Vector
 from readDataFCNN import readDataFCNN
+from tensorboardX import SummaryWriter
 #device name
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-#device = 'cpu'
+
 #hyper
 input_size =  2304 #48x48
-
 num_classes = 7
-num_epochs = 20
+num_epochs = 100
 batch_size = 32#100
 learning_rate = 0.001
 Leaning_csv = '../../dataset/LearningTest.csv'
 test_csv_file = '../../dataset/PublicTest.csv'
 
+writer = SummaryWriter()
 
 train_dataset = readDataFCNN(Leaning_csv)
+test_dataset = readDataFCNN(test_csv_file)
 
 train_loader = torch.utils.data.DataLoader(dataset=train_dataset,
                                            batch_size=batch_size,
                                            shuffle=True)
-test_dataset = readDataFCNN(test_csv_file)
 
 test_loader = torch.utils.data.DataLoader(dataset=test_dataset,
                                            batch_size=batch_size,
@@ -54,10 +56,32 @@ model.to(device)
 criterion = nn.CrossEntropyLoss()
 optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
 
-#traning
+
+def validate(model, test_loader, epoch):
+    with torch.no_grad():
+        model.eval()
+        n_correct = 0
+        n_samples = 0
+        for labels, images in test_loader:
+            images = images.to(device)
+            labels = labels.to(device)
+            outputs = model(images)
+
+            _, predictions = torch.max(outputs, 1)
+            n_samples += labels.shape[0]
+            n_correct += (predictions == labels).sum().item()
+
+        acc = 100.0 * n_correct / n_samples
+        print(f'Accuracy of the network on the test images: {acc} % in the Epoch {epoch+1}')
+        writer.add_scalar('Validation Accuracy', acc, epoch)
+
+
+    #traning
 n_total = len(train_loader)
 for epoch in range(num_epochs):
     model.train()
+    n_correct = 0
+    n_samples = 0
     for i, (labels, images) in enumerate(train_loader):
         images = images.to(device)
         labels = labels.to(device)
@@ -70,25 +94,24 @@ for epoch in range(num_epochs):
         loss.backward()
         optimizer.step()
 
-        if(i+1) % 100 == 0:
-            print(f"epoch {epoch +1} / { num_epochs}, step {i+1}/{n_total}, loss: {loss}")
-
-    print(f"Finish epoch {epoch + 1} of traning")
-    #test
-#torch.save(model,'feed-Forward.pth')
-torch.save(model.state_dict(), 'feed-Forward.pth')
-with torch.no_grad():
-    model.eval()
-    n_correct = 0
-    n_samples = 0
-    for labels, images in test_loader:
-        images = images.to(device)
-        labels = labels.to(device)
-        outputs = model(images)
-
-        _, predictions = torch.max(outputs,1)
+        #calculate traning accuracy
+        _, predictions = torch.max(outputs, 1)
         n_samples += labels.shape[0]
         n_correct += (predictions == labels).sum().item()
 
+        if(i+1) % 100 == 0:
+            print(f"epoch {epoch +1} / { num_epochs}, step {i+1}/{n_total}, loss: {loss}")
+
     acc = 100.0 * n_correct / n_samples
-    print(f'Accuracy of the network on the  test images: {acc} %')
+
+    print(f"Finish epoch {epoch + 1} of traning with tracc {acc} %")
+    writer.add_scalar('Training Accuracy', acc, epoch)
+    #test
+    if epoch % 20 == 0 and epoch != 0:
+        validate(model, test_loader,epoch)
+
+#torch.save(model,'feed-Forward.pth')
+torch.save(model.state_dict(), 'feed-Forward.pth')
+validate(model,test_loader,99)
+
+
