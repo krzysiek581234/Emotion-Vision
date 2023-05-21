4e13b35c5a658887959a3dae6ca2971176dfdf69
mateusz29 <68618996+mateusz29@users.noreply.github.com>
2023-05-21 18:05:30 +0200

    Update interface.py

--- interface.py
+++ interface.py
@@ -7,120 +7,139 @@ from NeuralNet import NeuralNet
 import torch
 
 selected_image = None
+found_faces = []
+coords = []
 
 def cnn_button_click():
-    if selected_image is None:
+    if found_faces is None:
         print("No image selected.")
         return
+    else:
+        cnn_detect_emotions()
 
-    cnn(selected_image)
-
-def cnn(image):
-    model = CNN()  # Create an instance of the model
-    model.load_state_dict(torch.load('./model.pth'))
+def cnn_detect_emotions():
+    emotions = ['Angry', 'Disgust', 'Fear', 'Happy', 'Sad', 'Surprise', 'Neutral']
+    device = 'cuda' if torch.cuda.is_available() else 'cpu'
+    model = CNN().to(device)
+    model.load_state_dict(torch.load('./model.pth', map_location=torch.device('cpu')))
     model.eval()
-    # Preprocess the image
-    image = cv2.resize(image, (48, 48), interpolation=cv2.INTER_AREA)
-    image = torch.from_numpy(image).unsqueeze(0).unsqueeze(0).float()
+    for i, face in enumerate(found_faces):
+        with torch.no_grad():
+            output = model(face)
+            probabilities = torch.softmax(output, dim=1)
+            emotion_index = torch.argmax(probabilities).item()
+            emotion_label = emotions[emotion_index]
+        x, y = coords[i][0], coords[i][1] 
+        cv2.putText(selected_image, emotion_label, (x, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (0, 255, 0), 2)
 
-    # Forward pass through the model
-    with torch.no_grad():
-        output = model(image)
-        _, predicted = torch.max(output, 1)
+    emotions_image = cv2.cvtColor(selected_image, cv2.COLOR_BGR2RGB)
+    emotions_image = Image.fromarray(emotions_image)
+    emotions_image_tk = ImageTk.PhotoImage(emotions_image)
+    display_image_label.configure(image=emotions_image_tk)
+    display_image_label.image = emotions_image_tk
 
-    emotions = {0: "Angry", 1: "Disgust", 2: "Fear", 3: "Happy", 4: "Sad", 5: "Surprise", 6: "Neutral"}
-    predicted_emotion = emotions[predicted.item()]
 
-    predicted_label.configure(text="Predicted Emotion: " + predicted_emotion)
-
+def contains_eye(face, eyes):
+    face_x, face_y, face_w, face_h = face
+    for (x, y, w, h) in eyes:
+        if x > face_x and x + w < face_x + face_w and y > face_y and y + h < face_y + face_h:
+            return True
+    return False
+    
 def detect_face():
+    #global selected_image, found_faces, coords
+    found_faces.clear()
+    coords.clear()
+    eyes_cascade = cv2.CascadeClassifier('haarcascade_eye.xml')
+    face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+    device = 'cuda' if torch.cuda.is_available() else 'cpu'
+
+    # convert chosen image to grayscale
+    gray = cv2.cvtColor(selected_image, cv2.COLOR_BGR2GRAY)
+
+    # eyes and faces detection
+    eyes = eyes_cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=3)
+    faces = face_cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=5, minSize=(30, 30))
+
+    # mark the eyes on the image
+    for (x, y, w, h) in eyes:
+        cv2.rectangle(selected_image, (x, y), (x + w, y + h), (255, 0, 0), 2)
+
+    # mark the faces on the image
+    for face in faces:
+        x, y, w, h = face
+        accept_face = contains_eye(face, eyes)
+        if accept_face:
+            # extract the region of interest (face) from the image
+            face_roi = gray[y:y+h, x:x+w]
+            # resize the image
+            face_roi = cv2.resize(face_roi, (48, 48))
+            face_roi = face_roi.reshape(1, 1, 48, 48)
+            face_roi = face_roi.astype("float32") / 255.0
+            # convert face_roi to a PyTorch tensor
+            face_tensor = torch.from_numpy(face_roi)
+            face_tensor = face_tensor.to(device)
+            found_faces.append(face_tensor)
+            coords.append((x, y))
+        #draw the rectangle that contains a face
+        cv2.rectangle(selected_image, (x, y), (x+w, y+h), (0, 0, 255), 2)
+    
+    haar_image = cv2.cvtColor(selected_image, cv2.COLOR_BGR2RGB)
+    haar_image = Image.fromarray(haar_image)
+    haar_image_tk = ImageTk.PhotoImage(haar_image)
+    display_image_label.configure(image=haar_image_tk)
+    display_image_label.image = haar_image_tk
+    
+def choose_image():
     global selected_image
+    found_faces.clear()
+    coords.clear()
     file_path = filedialog.askopenfilename()
     if not file_path:
         return
-
+    
+    # load the chosen image and display it on the window
     image = cv2.imread(file_path)
-    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-    face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
-    faces = face_cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=5, minSize=(30, 30))
-    original_image = image.copy()
+    selected_image = image.copy() 
+    chosen_image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+    chosen_image = Image.fromarray(chosen_image)
+    chosen_image_tk = ImageTk.PhotoImage(chosen_image)
+    display_image_label.configure(image=chosen_image_tk)
+    display_image_label.image = chosen_image_tk
 
-    for (x, y, w, h) in faces:
-        cv2.rectangle(image, (x, y), (x+w, y+h), (0, 0, 255), 2)
-        red_square_area = original_image[y:y+h, x:x+w]
-        red_square_area = cv2.resize(red_square_area, (48, 48), interpolation=cv2.INTER_AREA)
-        red_square_area = cv2.cvtColor(red_square_area, cv2.COLOR_BGR2GRAY)
-        selected_image = red_square_area.copy()
-
-    original_image = cv2.cvtColor(original_image, cv2.COLOR_BGR2RGB)
-    original_image = Image.fromarray(original_image)
-    original_image = original_image.resize((300, 300), Image.Resampling.LANCZOS)
-    original_image_tk = ImageTk.PhotoImage(original_image)
-
-    processed_image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-    processed_image = Image.fromarray(processed_image)
-    processed_image = processed_image.resize((300, 300), Image.Resampling.LANCZOS)
-    processed_image_tk = ImageTk.PhotoImage(processed_image)
-
-    original_label.configure(image=original_image_tk)
-    original_label.image = original_image_tk
-    processed_label.configure(image=processed_image_tk)
-    processed_label.image = processed_image_tk
-
-    red_square_area = cv2.cvtColor(red_square_area, cv2.COLOR_BGR2RGB)
-    red_square_area = Image.fromarray(red_square_area)
-    red_square_area = red_square_area.resize((300, 300), Image.Resampling.LANCZOS)
-    red_square_area_tk = ImageTk.PhotoImage(red_square_area)
-
-    extracted_label.configure(image=red_square_area_tk)
-    extracted_label.image = red_square_area_tk
+    # update the window geometry based on the image size
+    window_width = chosen_image.width + 20
+    window_height = chosen_image.height + 20
+    window.geometry(f"{window_width}x{window_height}")
 
 window = tk.Tk()
 window.title("Face Detection")
 
-window.geometry("990x540")
-window.resizable(False, False)
+window.geometry("960x540")
+window.resizable(True, True)
 
-file_button = tk.Button(window, text="Choose Image", command=detect_face)
+buttons_frame = tk.Frame(window)
+buttons_frame.pack(side="right", padx=10)
+
+file_button = tk.Button(buttons_frame, text="Choose Image", command=choose_image)
 file_button.pack(pady=10)
+
+face_detection_button = tk.Button(buttons_frame, text="Face Detection", command=detect_face)
+face_detection_button.pack(pady=10)
+
+cnn_button = tk.Button(buttons_frame, text="CNN", command=cnn_button_click)
+cnn_button.pack(pady=10)
+
+feed_forward_button = tk.Button(buttons_frame, text="Feed Forward")
+feed_forward_button.pack(pady=10)
+
+transfer_learning_button = tk.Button(buttons_frame, text="Transfer Learning")
+transfer_learning_button.pack(pady=10)
 
 labels_frame = tk.Frame(window)
 labels_frame.pack()
-                   
-text_label1 = tk.Label(labels_frame, text="Original Image", font=("Arial", 12))
-text_label1.pack(side="left", padx=100)
 
-text_label2 = tk.Label(labels_frame, text="Processed Image", font=("Arial", 12))
-text_label2.pack(side="left", padx=100)
-
-text_label3 = tk.Label(labels_frame, text="Extracted Area", font=("Arial", 12))
-text_label3.pack(side="left", padx=100)
-
-placeholder_image = Image.new("RGB", (300, 300), "white")
-placeholder_image_tk = ImageTk.PhotoImage(placeholder_image)
-
-pictures_frame = tk.Frame(window)
-pictures_frame.pack()
-
-original_label = tk.Label(pictures_frame, image=placeholder_image_tk)
-original_label.pack(side="left")
-
-processed_label = tk.Label(pictures_frame, image=placeholder_image_tk)
-processed_label.pack(side="left")
-
-extracted_label = tk.Label(pictures_frame, image=placeholder_image_tk)
-extracted_label.pack(side="left")
-
-buttons_frame = tk.Frame(window)
-buttons_frame.pack(pady=10)
-
-cnn_button = tk.Button(buttons_frame, text="CNN", command=cnn_button_click)
-cnn_button.pack(side="left", padx=10)
-
-feed_forward_button = tk.Button(buttons_frame, text="Feed Forward")
-feed_forward_button.pack(side="left", padx=10)
-
-predicted_label = tk.Label(window, text="Predicted Emotion: ", font=("Arial", 12))
-predicted_label.pack(pady=10)
+display_image_label = tk.Label(window)
+display_image_label.pack(padx=10, pady=10)
 
 window.mainloop()
